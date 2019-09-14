32f7bb9dc6272ffefc78b53fc43607013daef28b
Martín Lombardo <mlomb@users.noreply.github.com>
2019-09-14 13:11:04 -0300

    validatePath & validateTuple w/ testing

--- test/index.js
+++ test/index.js
@@ -30,6 +30,9 @@ function expectInvalidNamespacePath(actual) {
 function expectNaN(actual) {
     return expect(actual).to.eventually.rejectedWith("is NaN");
 }
+function expectPath(namespace, key, actual) {
+    return expect(actual).to.eventually.have.property('path').to.be.equals(typeof namespace === "undefined" ? key : `${namespace}/${key}`);
+}
 function expectExistingKey(actual) {
     return expect(actual).to.eventually.have.property('value').not.null;
 }
@@ -40,29 +43,30 @@ function expectStatusCode(code, actual) {
 describe('CountAPI', function() {
     this.timeout(10000);
 
+    describe('validate path', function() {
+        it('undefined undefined', () => expectMissingKey(countapi.validatePath()));
+        it('empty key', () => expectInvalidKeyPath(countapi.validatePath('')));
+        it('empty key, namespace', () => expectInvalidKeyPath(countapi.validatePath('', '')));
+        it('invalid key', () => expectInvalidKeyPath(countapi.validatePath(INVALID_KEY)));
+        it('invalid namespace', () => expectInvalidNamespacePath(countapi.validatePath(INVALID_NAMESPACE, 'test')));
+        it('undefined namespace', () => expectPath(undefined, 'test', countapi.validatePath(undefined, 'test')));
+        it('undefined key', () => expectPath(undefined, 'test', countapi.validatePath('test', undefined)));
+    });
+
     describe('get', function() {
-        it('undefined key', () => expectMissingKey(countapi.get()));
-        it('empty key', () => expectInvalidKeyPath(countapi.get('')));
-        it('invalid key', () => expectInvalidKeyPath(countapi.get(INVALID_KEY)));
-        it('undefined namespace', () => expectExistingKey(countapi.get(undefined, 'test')));
-        it('invalid namespace', () => expectInvalidNamespacePath(countapi.get(INVALID_NAMESPACE, 'test')));
-        it('key in default namespace', () => expectExistingKey(countapi.get('test')));
-        it('key in test namespace', () => expectExistingKey(countapi.get('test', 'test')));
-        it('non existing key', () => expectStatusCode(404, countapi.get(uuidv4())));
+        it('key in default namespace', () => expectStatusCode(200, countapi.get('test')));
+        it('key in test namespace', () => expectStatusCode(200, countapi.get('test', 'test')));
+        it('non existing', () => expectStatusCode(404, countapi.get(uuidv4())));
     });
 
     describe('set', function() {
-        it('key, value undefined', () => expectMissingKey(countapi.set()));
-        it('empty key', () => expectMissingKey(countapi.set()));
-        it('value undefined', () => expectMissingKeyOrValue(countapi.set('test')));
-        it('NaN value', () => expectNaN(countapi.set('test', '3.14')));
-        it('key undefined with NaN value', () => expectNaN(countapi.set(undefined, '3.14')));
-        it('key undefined', () => expectMissingKey(countapi.set(undefined, 3)));
-        it('invalid key', () => expectInvalidKeyPath(countapi.set(INVALID_KEY, 3)));
-        it('invalid namespace', () => expectInvalidNamespacePath(countapi.set(INVALID_NAMESPACE, 'test', 3)));
-        it('key, namespace undefined', () => expectMissingKey(countapi.set(undefined, undefined, 3)));
-        it('set non existing', () => expectStatusCode(404, countapi.set('test', uuidv4(), 3)));
-        it('set without enable_reset', () => expectStatusCode(403, countapi.set('d7b97e5f-69a4-4933-8928-b9ee92f6b6ca', 3)));
-        it('set in default namespace', () => expectStatusCode(200, countapi.set('default', 'c23ce4fa-4b78-4cbb-b53a-da3c2c38bb18', 3)));
+        it('non existing', () => expectStatusCode(404, countapi.set('test', uuidv4(), 3)));
+        it('without enable_reset', () => expectStatusCode(403, countapi.set('d7b97e5f-69a4-4933-8928-b9ee92f6b6ca', 3)));
+        it('in default namespace', () => expectStatusCode(200, countapi.set('default', 'c23ce4fa-4b78-4cbb-b53a-da3c2c38bb18', 3)));
+    });
+    
+    describe('update', function() {
+        it('non existing', () => expectStatusCode(404, countapi.set('test', uuidv4(), 3)));
+        // TODO
     });
 });
