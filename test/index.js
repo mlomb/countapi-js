69465f14701cc1cdf9900deeddb218c3e53b5ea5
Martín Lombardo <mlomb@users.noreply.github.com>
2019-09-14 01:11:22 -0300

    set testing

--- test/index.js
+++ test/index.js
@@ -16,7 +16,10 @@ const INVALID_NAMESPACE = 'aa';
 ///
 
 function expectMissingKey(actual) {
-    return expect(actual).to.eventually.rejectedWith("Missing Key");
+    return expect(actual).to.eventually.rejectedWith("Missing key");
+}
+function expectMissingKeyOrValue(actual) {
+    return expect(actual).to.eventually.rejectedWith("Missing key or value");
 }
 function expectInvalidKeyPath(actual) {
     return expect(actual).to.eventually.rejectedWith("Key must match");
@@ -24,14 +27,19 @@ function expectInvalidKeyPath(actual) {
 function expectInvalidNamespacePath(actual) {
     return expect(actual).to.eventually.rejectedWith("Namespace must match");
 }
+function expectNaN(actual) {
+    return expect(actual).to.eventually.rejectedWith("is NaN");
+}
 function expectExistingKey(actual) {
     return expect(actual).to.eventually.have.property('value').not.null;
 }
-function expectNonExistingKey(actual) {
-    return expect(actual).to.eventually.have.property('value').null;
+function expectStatusCode(code, actual) {
+    return expect(actual).to.eventually.have.property('status').equal(code);
 }
 
 describe('CountAPI', function() {
+    this.timeout(10000);
+
     describe('get', function() {
         it('undefined key', () => expectMissingKey(countapi.get()));
         it('empty key', () => expectInvalidKeyPath(countapi.get('')));
@@ -40,6 +48,21 @@ describe('CountAPI', function() {
         it('invalid namespace', () => expectInvalidNamespacePath(countapi.get(INVALID_NAMESPACE, 'test')));
         it('key in default namespace', () => expectExistingKey(countapi.get('test')));
         it('key in test namespace', () => expectExistingKey(countapi.get('test', 'test')));
-        it('non existing key', () => expectNonExistingKey(countapi.get(uuidv4())));
+        it('non existing key', () => expectStatusCode(404, countapi.get(uuidv4())));
+    });
+
+    describe('set', function() {
+        it('key, value undefined', () => expectMissingKey(countapi.set()));
+        it('empty key', () => expectMissingKey(countapi.set()));
+        it('value undefined', () => expectMissingKeyOrValue(countapi.set('test')));
+        it('NaN value', () => expectNaN(countapi.set('test', '3.14')));
+        it('key undefined with NaN value', () => expectNaN(countapi.set(undefined, '3.14')));
+        it('key undefined', () => expectMissingKey(countapi.set(undefined, 3)));
+        it('invalid key', () => expectInvalidKeyPath(countapi.set(INVALID_KEY, 3)));
+        it('invalid namespace', () => expectInvalidNamespacePath(countapi.set(INVALID_NAMESPACE, 'test', 3)));
+        it('key, namespace undefined', () => expectMissingKey(countapi.set(undefined, undefined, 3)));
+        it('set non existing', () => expectStatusCode(404, countapi.set('test', uuidv4(), 3)));
+        it('set without enable_reset', () => expectStatusCode(403, countapi.set('d7b97e5f-69a4-4933-8928-b9ee92f6b6ca', 3)));
+        it('set in default namespace', () => expectStatusCode(200, countapi.set('default', 'c23ce4fa-4b78-4cbb-b53a-da3c2c38bb18', 3)));
     });
 });
